2d0a88e4b2a3429dbddc8b555a01aff497c4904b
Nooh Kader <nooh.kader@gmail.com$ ssh-keygen -t rsa -b 4096 -C nooh.kader@gmail.com>
2019-11-29 00:15:07 -0800

    removed the directories

--- build.py
+++ build.py
@@ -1,21 +1,21 @@
 # First, get the template files
 
-top = open('templates/templates_top.html').read()
-bottom = open('templates/templates_bottom.html').read()
+top = open('templates_top.html').read()
+bottom = open("templates_bottom.html').read()
 
 # Read in index HTML code
-indexcontent = open('content/index_content.html').read()
+indexcontent = open('index_content.html').read()
 
 # Combine template HTML code with content HTML code
 index_html = top + indexcontent + bottom
-open('docs/index.html', 'w+').write(index_html)
+open('index.html', 'w+').write(index_html)
 
-contactcontent = open('cotent/contact_content.html').read()
+contactcontent = open('contact_content.html').read()
 contact_html = top + contactcontent + bottom
-open('docs/contact.html', 'w+').write(index_html)
+open('contact.html', 'w+').write(index_html)
 
-projectcontent = open('content/project_content').read()
+projectcontent = open('project_content').read()
 projects_html = top + projectcontent + bottom
-open('docs/projects.html', 'w+').write(index_html)
+open('projects.html', 'w+').write(index_html)
 
 
