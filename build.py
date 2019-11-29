a5863959c03fcf10915e904ad3bbf8174b4b8512
Nooh Kader <nooh.kader@gmail.com$ ssh-keygen -t rsa -b 4096 -C nooh.kader@gmail.com>
2019-11-29 00:07:17 -0800

    update

--- build.py
+++ build.py
@@ -1,22 +1,19 @@
 # First, get the template files
-from pathlib import Path
 
-templates_top = Path("templates/template_top.html")
-
-top = open('templates_top.html').read()
-bottom = open('templates_bottom.html').read()
+top = open('templates/templates_top.html').read()
+bottom = open('templates/templates_bottom.html').read()
 
 # Read in index HTML code
-indexcontent = open('index_content.html').read()
+indexcontent = open('content/index_content.html').read()
 
 # Combine template HTML code with content HTML code
 index_html = top + indexcontent + bottom
 open('index.html', 'w+').write(index_html)
 
-contactcontent = open('contact_content.html').read()
+contactcontent = open('cotent/contact_content.html').read()
 contact_html = top + contactcontent + bottom
 
-projectcontent = open('project_content').read()
+projectcontent = open('content/project_content').read()
 projects_html = top + projectcontent + bottom
 
 
