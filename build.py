4c4697ef1b3cf670a3a746a4dd32bc8648b9ee82
Nooh Kader <nooh.kader@gmail.com$ ssh-keygen -t rsa -b 4096 -C nooh.kader@gmail.com>
2019-11-29 00:10:21 -0800

    updated

--- build.py
+++ build.py
@@ -8,12 +8,14 @@ indexcontent = open('content/index_content.html').read()
 
 # Combine template HTML code with content HTML code
 index_html = top + indexcontent + bottom
-open('index.html', 'w+').write(index_html)
+open('docs/index.html', 'w+').write(index_html)
 
 contactcontent = open('cotent/contact_content.html').read()
 contact_html = top + contactcontent + bottom
+open('docs/contact.html', 'w+').write(index_html)
 
 projectcontent = open('content/project_content').read()
 projects_html = top + projectcontent + bottom
+open('docs/projects.html', 'w+').write(index_html)
 
 
